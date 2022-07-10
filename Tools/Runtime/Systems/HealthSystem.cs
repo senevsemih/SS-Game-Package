8b371bdd46d57ab2acb4ccd7517d0442ea92f718
Semih Şenev <senevsemih@gmail.com>
2022-07-10 11:47:21 +0300

    Health system updated and test script added.

--- Tools/Runtime/Systems/HealthSystem.cs
+++ Tools/Runtime/Systems/HealthSystem.cs
@@ -8,12 +8,9 @@ using UnityEngine.UI;
 namespace GamePackage.Runtime.Systems
 {
     [Serializable]
-    public class HealthSystem
+    public class HealthSystem : MonoBehaviour
     {
-        // Mono Behaviour.
-		// Regen.
-		
-		public event Action DidDie;
+        public event Action DidDie;
         
         [ShowInInspector, ReadOnly] private bool _isAlive;
         [Space]
@@ -22,12 +19,16 @@ namespace GamePackage.Runtime.Systems
         [Space]
         [ShowInInspector, ReadOnly] private bool _hasUI;
         [ShowInInspector, ReadOnly] private bool _hasAnimation;
+        [ShowInInspector, ReadOnly] private float _animationDuration;
+        [Space]
         [ShowInInspector, ReadOnly] private Image _healthBarImage;
         [ShowInInspector, ReadOnly] private TMP_Text _healthText;
         
         private const int MIN_HEALTH = 0;
-        private float _animationDuration = 1f;
-        private float _timer = 0f;
+        private float _timer;
+        private int _storedHealth;
+
+        private Coroutine _healthTransitionRoutine;
         
         private int Health
         {
@@ -45,12 +46,23 @@ namespace GamePackage.Runtime.Systems
                 DidDie?.Invoke();
             }
         }
-		
-        public HealthSystem(
+
+        private int StoredHealth
+        {
+            get => _storedHealth;
+            set
+            {
+                _storedHealth = value;
+                _storedHealth = Mathf.Clamp(_storedHealth, MIN_HEALTH, _maxHealth);
+            }
+        }
+
+        public void InitialSettings(
             int initialHealth, 
             int maxHealth = 100, 
             bool hasUI = false,
             bool hasAnimation = false,
+            float animationDuration = 0.5f,
             Image healthBarImage = null, 
             TMP_Text healthText = null)
         {
@@ -58,48 +70,44 @@ namespace GamePackage.Runtime.Systems
             
             _health = initialHealth;
             _maxHealth = maxHealth;
+            _storedHealth = _health;
             
             _hasUI = hasUI;
             _hasAnimation = hasAnimation;
+            _animationDuration = animationDuration;
             _healthBarImage = healthBarImage;
             _healthText = healthText;
 		
             UIUpdate();
         }
-		
+        
         public void FullHealth()
         {
             Health = _maxHealth;
+            StoredHealth = _maxHealth;
         }
 		
         public void IncreaseMaxHealth(int value)
         {
-            Health += value;
             _maxHealth += value;
+            Health += value;
+            StoredHealth += value;
         }
 		
         public void GetHit(int damage)
         {
             var currentValue = Health;
-            var nextValue = Health -= damage;
-            
-            //UIAnimation(currentValue, nextValue);
-            //CoroutineRunner.Run(Routine(currentValue, nextValue));
-        }
-        
-        private void UIAnimation(int currentValue, int nextValue)
-        {
-            if (!_hasAnimation) return;
-		
-            while (_timer < _animationDuration && _isAlive)
+            StoredHealth -= damage;
+
+            if (_healthTransitionRoutine != null)
             {
-                _timer += Time.deltaTime;
-                var tVal = Mathf.InverseLerp(0, _animationDuration, _timer);
-                Health = (int)Mathf.Lerp(currentValue, nextValue, tVal);
+                StopCoroutine(_healthTransitionRoutine);
+                _timer = 0;
             }
+            _healthTransitionRoutine = StartCoroutine(HealthTransitionRoutine(currentValue, _storedHealth));
         }
         
-        private IEnumerator Routine(int currentValue, int nextValue)
+        private IEnumerator HealthTransitionRoutine(int currentValue, int nextValue)
         {
             if (!_hasAnimation) yield return null;
             
