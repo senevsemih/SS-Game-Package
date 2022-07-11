e3962b60a7e7b188a50113152ef689cbd6c12fa2
Semih Şenev <senevsemih@gmail.com>
2022-07-11 18:39:16 +0300

    Health system refactor

--- Tools/Tests/Runtime/HealthSystemTest.cs
+++ Tools/Tests/Runtime/HealthSystemTest.cs
@@ -4,55 +4,59 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthSystemTest : MonoBehaviour
+namespace GamePackage.Tests.Runtime
 {
-    [SerializeField] private int _InitialHealth;
-    [SerializeField] private int _MaxHealth;
-    [SerializeField] private float _AnimationDuration;
-    [Space] 
-    [SerializeField] private bool _HasUI;
-    [SerializeField] private bool _HasAnimation;
-    [Space]
-    [SerializeField] private Image _HealthImage;
-    [SerializeField] private TMP_Text _HealthText;
-    
-    [ShowInInspector, ReadOnly] private HealthSystem _health;
-    
-    private void Awake()
+    public class HealthSystemTest : MonoBehaviour
     {
-        if (_health) return; 
-        _health = gameObject.AddComponent<HealthSystem>();
-        _health.InitialSettings(100);
-        
-        _health.DidDie += HealthOnDidDie;
-    }
+        [SerializeField] private int _InitialHealth;
+        [SerializeField] private int _MaxHealth;
+        [SerializeField] private float _AnimationDuration;
+        [Space] 
+        [SerializeField] private bool _HasUI;
+        [SerializeField] private bool _HasAnimation;
+        [Space] 
+        [SerializeField] private Image _HealthImage;
+        [SerializeField] private TMP_Text _HealthText;
 
-    [Button]
-    private void UpdateValues()
-    {
-        _health.InitialSettings(_InitialHealth, _MaxHealth, _HasUI, _HasAnimation, _AnimationDuration, _HealthImage, _HealthText);
-    }
+        [ShowInInspector, ReadOnly] private HealthSystem _health;
 
-    [Button]
-    private void GetHit(int value)
-    {
-        _health.GetHit(value);
-    }
+        private void Awake()
+        {
+            if (_health) return;
+            _health = gameObject.AddComponent<HealthSystem>();
+            _health.InitialSettings(100);
 
-    [Button]
-    private void IncreaseMaxHealth(int value)
-    {
-        _health.IncreaseMaxHealth(value);
-    }
+            _health.DidDie += HealthOnDidDie;
+        }
 
-    [Button]
-    private void FullHealth()
-    {
-        _health.FullHealth();
-    }
-    
-    private void HealthOnDidDie()
-    {
-        Debug.Log("Dead");
+        [Button]
+        private void UpdateValues()
+        {
+            _health.InitialSettings(_InitialHealth, _MaxHealth, _HasUI, _HasAnimation, _AnimationDuration, _HealthImage,
+                _HealthText);
+        }
+
+        [Button]
+        private void GetHit(int value)
+        {
+            _health.GetHit(value);
+        }
+
+        [Button]
+        private void IncreaseMaxHealth(int value)
+        {
+            _health.IncreaseMaxHealth(value);
+        }
+
+        [Button]
+        private void FullHealth()
+        {
+            _health.FullHealth();
+        }
+
+        private void HealthOnDidDie()
+        {
+            Debug.Log("Dead");
+        }
     }
 }
