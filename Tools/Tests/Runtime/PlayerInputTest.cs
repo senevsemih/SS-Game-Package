4529119809855a477884ad5d38b80133ecfbf16d
Semih Şenev <senevsemih@gmail.com>
2022-07-20 15:27:28 +0300

    Input and stack tests added.

---
+++ Tools/Tests/Runtime/PlayerInputTest.cs
@@ -0,0 +1,34 @@
+﻿using GamePackage.Runtime.Input;
+using UnityEngine;
+
+namespace GamePackage.Tests.Runtime
+{
+    public class PlayerInputTest : MonoBehaviour
+    {
+        [SerializeField] private InputDragHandler _Input;
+        [SerializeField] private float _Speed;
+        
+        private Transform _transform;
+        
+        private void Awake()
+        {
+            _transform = transform;
+            _Input.DidDrag += InputOnDidDrag;
+            _Input.DidDragEnd += InputOnDidDragEnd;
+        }
+        
+        private void InputOnDidDrag(Vector3 direction)
+        {
+            var pos = _transform.position;
+            var newX = pos.x + direction.x * _Speed;
+            var newPos = new Vector3(newX, pos.y, pos.z);
+            
+            _transform.position = newPos;
+        }
+        
+        private void InputOnDidDragEnd()
+        {
+            Debug.Log("Release");
+        }
+    }
+}
---
+++ Tools/Tests/Runtime/PlayerStackTest.cs
@@ -0,0 +1,32 @@
+﻿using GamePackage.Runtime.Physics;
+using GamePackage.Runtime.Stack;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GamePackage.Tests.Runtime
+{
+    public class PlayerStackTest : MonoBehaviour
+    {
+        [SerializeField, Required] private StackConfig _StackConfig = new()
+        {
+            StackCapacity = 100,
+            StackDirection = StackDirection.Forward,
+            StackOffset = 1,
+            StackMaxSpeed = 20,
+            StackSpeedDecreaseRate = 1
+        };
+        
+        [SerializeField] private Transform _StackStartPosition;
+        [SerializeField, Required] private PhysicsListener _PhysicsListener;
+        
+        private StackFormation _forwardStackFormation;
+
+        private void Start()
+        {
+            _forwardStackFormation = new StackFormation(
+                _StackConfig,
+                _StackStartPosition,
+                _PhysicsListener);
+        }
+    }
+}
